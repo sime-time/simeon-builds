87042314fe8ff8e7abd03bccf34f02ecc02f41ec
Dunn, Simeon <simedunn01@gmail.com>
2025-01-30 16:25:54 -0500

    dropdown links

--- src/components/PageCard.tsx
+++ src/components/PageCard.tsx
@@ -10,7 +10,7 @@ export default function PageCard(props: PageCardProps) {
   return (
     <section class="glass-container w-full p-7 flex flex-col gap-5">
       <div class="flex justify-between items-center">
-        <h1 class="text-4xl font-semibold">{props.title}</h1>
+        <h1 class="text-3xl md:text-4xl font-semibold">{props.title}</h1>
         <Nav />
       </div>
       {props.children}
--- src/components/PersonalLink.tsx
+++ src/components/PersonalLink.tsx
@@ -9,14 +9,14 @@ interface PersonalLinkProps {
 
 export default function PersonalLink(props: PersonalLinkProps) {
   return (
-    <div class="flex items-center gap-4  hover:text-yellow-300 transition-all">
-      <a href={props.href} target="_blank" class="border border-slate-100/20 rounded-xl p-3">
+    <a href={props.href} target="_blank" class="flex items-center gap-4  hover:text-yellow-300 transition-all">
+      <div class="border border-slate-100/20 rounded-xl p-3">
         {props.icon}
-      </a>
+      </div>
       <div class="flex flex-col text-sm">
         <h3 class="uppercase text-xs text-white opacity-60">{props.title}</h3>
-        <a href={props.href} target="_blank">{props.name}</a>
+        <div>{props.name}</div>
       </div>
-    </div>
+    </a>
   );
 }
